03f462f2925f82392cf998debc2e4c62b147adbe
tansoz <tantoz@icloud.com>
2020-09-20 02:28:51 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 cd /etc/nginx/conf.d/
 rm -f proxy.conf
 wget "http://jp.cdn.rommhui.com/https/raw.githubusercontent.com/tansoz/NodeConf/master/proxy.conf"
+sed 's/listen 80;/listen $1;/' ./proxy.conf
 nginx -s reload
