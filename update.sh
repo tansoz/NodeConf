21503ea6ba303f4619125162cb4c7464d09e0c95
tansoz <tantoz@icloud.com>
2020-09-20 11:50:07 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,13 @@
-cd /etc/nginx/conf.d/
+if [ ! -d "/etc/nginx/conf.d/" ];then
+  cd /etc/nginx/conf.d/
+else
+  if [ ! -d "/www/server/panel/vhost/nginx/" ];then
+    cd /www/server/panel/vhost/nginx/
+  else
+    echo "You don't have installed nginx on the machine!"
+    exit 0
+  fi
+fi
 rm -f ./proxy.conf
 wget "http://jp.cdn.rommhui.com/https/raw.githubusercontent.com/tansoz/NodeConf/master/proxy.conf"
 sed -i "s/listen 80;/listen $1;/g" ./proxy.conf
