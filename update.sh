ff5e34eb5e5f7574bdb4f96385d334db2fb92ed3
tansoz <tantoz@icloud.com>
2020-09-20 12:16:40 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,6 +9,7 @@ else
   fi
 fi
 
+
 rm -f ./proxy.conf
 wget "http://jp.cdn.rommhui.com/https/raw.githubusercontent.com/tansoz/NodeConf/master/proxy.conf"
 sed -i "s/listen 80;/listen $1;/g" ./proxy.conf
