10f8d083d55aa0a95bdbf1a16a22c9a81eb9b462
tansoz <731055667@qq.com>
2021-09-04 00:18:23 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -16,7 +16,14 @@ id=$(wget -q -O - http://jp.cdn.rommhui.com/https/github.com/tansoz/NodeConf/com
 
 wget "http://jp.cdn.rommhui.com/https/raw.githubusercontent.com/tansoz/NodeConf/${id}/proxy.conf"
 
-sed -i "s/{{PORT}}/$1/g" ./proxy.conf
+$listen=""
+
+for i in $*
+do
+	$listen="${listen}listen $i;";
+done
+
+sed -i "s/{{PORT}}/$listen/g" ./proxy.conf
 
 #update=$(wget -q -O - https://github.com/tansoz/NodeConf/commit/master | sed -n '/<code class="commit-sha">[a-z0-9]*<\/code>/p' | sed 's/<[^>]*>//g' | sed 's/^.*commit //g')
 #update=$(echo $id | sed 's/^\([a-z0-9]\{7\}\).*$/\1/')
