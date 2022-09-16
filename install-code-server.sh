aee163258d8313efea7f4035f5e8a49281feb567
tansoz <731055667@qq.com>
2022-09-16 16:13:36 +0800

    Update install-code-server.sh

--- install-code-server.sh
+++ install-code-server.sh
@@ -17,8 +17,10 @@ mv ./vscode-server-linux-x64-web ./code-server
 mkdir -p ./code-server-data
 
 rm -f run-code-server.sh
+curl https://purge.jsdelivr.net/gh/tansoz/NodeConf@master/run-code-server.sh > /dev/null
 wget https://cdn.jsdelivr.net/gh/tansoz/NodeConf@master/run-code-server.sh
 chmod +x run-code-server.sh
+curl https://purge.jsdelivr.net/gh/tansoz/NodeConf@master/code-server.code-server > /dev/null
 wget https://cdn.jsdelivr.net/gh/tansoz/NodeConf@master/code-server.service
 
 mv code-server.service /etc/systemd/system/code-server.service
