89aa7a97f1e350dfe7df7f5abc8fb9b5fed0cd23
tansoz <731055667@qq.com>
2022-09-16 15:50:55 +0800

    Update run-code-server.sh

--- run-code-server.sh
+++ run-code-server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./code-server/node ./code-server/out/server-main.js --accept-server-license-terms -p 6262 --without-connection-token --server-data-dir ./code-server-data
+ROOT=/server
+
+$ROOT/code-server/node $ROOT/code-server/out/server-main.js --accept-server-license-terms -p 6262 --without-connection-token --server-data-dir $ROOT/code-server-data
