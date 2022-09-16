bfde6d9bee078e78ed243a63416fd08695238cc1
tansoz <731055667@qq.com>
2022-09-16 16:02:13 +0800

    Update run-code-server.sh

--- run-code-server.sh
+++ run-code-server.sh
@@ -2,4 +2,4 @@
 
 ROOT=/server
 
-$ROOT/code-server/node $ROOT/code-server/out/server-main.js --accept-server-license-terms -p 6262 --without-connection-token --server-data-dir $ROOT/code-server-data
+$ROOT/code-server/node $ROOT/code-server/out/server-main.js --accept-server-license-terms --port 6262 --log error --without-connection-token --server-data-dir $ROOT/code-server-data
