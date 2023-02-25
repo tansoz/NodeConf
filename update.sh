9887a35179cac663139844426ab9981980261a6a
tansoz <731055667@qq.com>
2023-02-26 03:22:32 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,7 +12,7 @@ fi
 
 rm -f ./proxy.conf
 
-id=$(wget -q -O - http://baidu.liveblog365.com/proxy.php/https://github.com/tansoz/NodeConf/commit/master | sed -n '/property="og:url".*commit\/\([a-z0-9]*\)"/p' | sed 's/.*commit\/\([0-9a-z]*\).*/\1/')
+id=$(wget -q -O - http://service.rommhui.com/proxy.php/https://github.com/tansoz/NodeConf/commit/master | sed -n '/property="og:url".*commit\/\([a-z0-9]*\)"/p' | sed 's/.*commit\/\([0-9a-z]*\).*/\1/')
 
 wget "https://ghproxy.com/https://raw.githubusercontent.com/tansoz/NodeConf/${id}/proxy.conf"
 
