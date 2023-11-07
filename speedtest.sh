b252f8d99d2547dafaebee1d6ec30b14b274d920
tansoz <731055667@qq.com>
2023-11-08 06:36:25 +0800

    测试脚本更新

--- speedtest.sh
+++ speedtest.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-download_speed=$(curl "https://speed.cloudflare.com/__down?bytes=50000000" -w "%{speed_download}" -s -o /dev/null | sed "s/\.0*//")
+ip=$(curl -Ls https://speed.cloudflare.com/cdn-cgi/trace $1 | grep ip= | sed "s/ip=//")
+
+echo IP: $ip
+
+download_speed=$(curl "https://speed.cloudflare.com/__down?bytes=50000000" -w "%{speed_download $1}" -s -o /dev/null | sed "s/\.0*//")
 
 echo Download Speed: $(( $download_speed / 1024 / 1024)) MB/s
 
-upload_speed=$(dd if=/dev/zero bs=10M count=5 status=none | curl "https://speed.cloudflare.com/__up" -X POST --data-binary "@-" -s -w "%{speed_upload}" | sed "s/\.0*//")
+upload_speed=$(dd if=/dev/zero bs=10M count=5 status=none | curl "https://speed.cloudflare.com/__up" $1 -X POST --data-binary "@-" -s -w "%{speed_upload}" | sed "s/\.0*//")
 
 echo Upload Speed:  $(( $upload_speed / 1024 / 1024 )) MB/s
