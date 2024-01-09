f08b6190c9e065d86c1683804336e7fa2677abd7
tansoz <731055667@qq.com>
2024-01-10 02:59:19 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ rm -f ./proxy.conf
 
 id=$(wget -q -O - http://service.rommhui.com/proxy.php/https://github.com/tansoz/NodeConf/commit/master | sed -n '/property="og:url".*commit\/\([a-z0-9]*\)"/p' | sed 's/.*commit\/\([0-9a-z]*\).*/\1/')
 
-wget "https://ghproxy.com/https://raw.githubusercontent.com/tansoz/NodeConf/${id}/proxy.conf"
+wget "http://service.rommhui.com/proxy.php/https://raw.githubusercontent.com/tansoz/NodeConf/${id}/proxy.conf"
 
 listen=""
 
